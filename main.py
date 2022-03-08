00b04bd1daa7fa58f055edb50290f14fdb2806f5
Ivan <buran05@icloud.com>
2022-03-08 11:18:10 +0300

    flask-wtf second task

--- main.py
+++ main.py
@@ -8,5 +8,17 @@ def index(title):
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<prof>')
+def training(prof: str):
+    if 'инженер' in prof.lower() or 'строитель' in prof.lower():
+        return render_template('training.html', title="Тренировки в полёте",
+                               place='Инженерные тренажеры',
+                               image_path=url_for('static', filename='img/tech_place.png'))
+    else:
+        return render_template('training.html', title="Тренировки в полёте",
+                               place='Научные симуляторы',
+                               image_path=url_for('static', filename='img/bio_place.png'))
+
+
 if __name__ == '__main__':
-    app.run(port=8000, host='127.0.0.1')
+    app.run(port=8080, host='127.0.0.1')
